7b0e96cca5be2ed6f5f622cbae9db4a341ba95b4
Sumit Rathore <166226632+SumitRathore23@users.noreply.github.com>
2025-08-07 10:21:14 +0000

    container config done

---
+++ Date_Time.js
@@ -0,0 +1,32 @@
+// Dates And Time
+
+/*let myDate = new Date();
+console.log("Current Date and Time: " + myDate);
+console.log("Current Date and Time: " + myDate);
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.toString());
+console.log(myDate.toTimeString());
+*/
+
+
+/*let myDate = new Date("2023-10-01");
+console.log("Date: " + myDate);*/
+
+
+let myDate = new Date;
+console.log(myDate.getDate());
+console.log(myDate.getDay());
+console.log(myDate.getFullYear());
+console.log(myDate.getHours());
+console.log(myDate.getMilliseconds());
+console.log(myDate.getMinutes());
+console.log(myDate.getMonth());
+console.log(myDate.getSeconds());
+console.log(myDate.getTimezoneOffset());
+console.log(myDate.getUTCDate());
+console.log(myDate.getUTCDay());
+console.log(myDate.getUTCFullYear());
+console.log(myDate.getUTCHours());
