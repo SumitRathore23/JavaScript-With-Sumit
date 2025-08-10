603f37ecf9c7f4e8fb8a9fc17d88ff0a8411773a
Sumit Rathore <166226632+SumitRathore23@users.noreply.github.com>
2025-08-10 15:07:26 +0000

    container config done

--- Array.js
+++ Array.js
@@ -1,4 +1,4 @@
-/*const myArr = [1, 2, 3, 4, 5];
+const myArr = [1, 2, 3, 4, 5];
 
 console.log(myArr[3]); // Output: 4
 console.log(myArr.length); // Output: 5
@@ -45,7 +45,7 @@ console.log(myArr2.slice(1, 4));
 console.log(myArr2.splice(1, 2));
  // Output: [2,3]  Original Array is modified
 console.log(myArr2); // Output: [1, 4, 5, 6]
-*/
+
 
 // Array Part 2
 
@@ -59,14 +59,14 @@ const superheroes = [...Marvel, ...DC];
 console.log(superheroes); // Output: ['Ironman', 'Thor', 'Hulk', 'Black Widow', 'Captain America', 'Superman', 'Batman', 'Wonder', 'Aquaman', 'Flash']
 
 // IsArray ,From, Of
-/*console.log(Array.isArray(superheroes)); // Output: true
+console.log(Array.isArray(superheroes)); // Output: true
 console.log(Array.from("Sumit")); // Output: ['S', 'u', 'm', 'i', 't']
 let sc1 = 100;
 let sc2 = 200;
 let sc3 = 300;
 let sc4 = 444;
 let sc5 = 555;
-console.log(Array.of(sc1,sc2,sc3,sc4,sc5)); // Output: [100, 200, 300, 444, 555]*/
+console.log(Array.of(sc1,sc2,sc3,sc4,sc5)); // Output: [100, 200, 300, 444, 555]
 
 const another_array = [2,3,4,5,6,6,[7,8,9,4,5,6],[4,5,6,8],[1,2,3,4]];
 const real_another_array = another_array.flat(Infinity);
