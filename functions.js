c4715084ca80bf4e21773e3a2eccdcede4079674
Sumit Rathore <166226632+SumitRathore23@users.noreply.github.com>
2025-08-14 16:55:52 +0000

    Basic Functions

---
+++ functions.js
@@ -0,0 +1,28 @@
+console.log("S");
+console.log("U");
+console.log("M");
+console.log("I");
+console.log("T");
+
+function sayMyName(){
+    console.log("S");
+    console.log("U");
+    console.log("M");
+    console.log("I");
+    console.log("T");
+}
+
+sayMyName()
+
+// Add Two Numbers
+
+function addTwoNumbers(number1,number2){
+    result= number1+number2
+    console.log(result)
+    return result
+}
+addTwoNumbers(2,3) //5
+addTwoNumbers(2,"a") // 2a
+addTwoNumbers(2,"4")// 24
+addTwoNumbers(2,null)// 2
+
