1b5c27cd4461183f3be62216d71e62fb78b27634
Sumit Rathore <166226632+SumitRathore23@users.noreply.github.com>
2025-08-15 16:56:22 +0000

    Basic Functions

--- functions.js
+++ functions.js
@@ -33,5 +33,37 @@ function loginUserName(username = "Shivam"){
     }
     return `${username} Just Logged In`
 }
-console.log(loginUserName());
-console.log(loginUserName("Sumit Rathore"));
+console.log(loginUserName());//Shivam Just Logged In
+console.log(loginUserName("Sumit Rathore"));//Sumit Rathore Just Logged In
+
+// Use In Projects like E-Commerse
+
+function calculateCartPrice(val1,val2,...num1){
+    return num1
+}
+console.log(calculateCartPrice(200,300,400,500));//[ 400, 500 ]
+
+// Any Object Use In function
+
+const user = {
+    userName : "Sumit",
+    Price : 1987
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.userName} and Price is ${anyobject.Price}`);
+}
+
+handleObject(user);// Username is Sumit and Price is 1987
+handleObject({
+    userName:"Sam",
+    Price: 789
+}) //Username is Sam and Price is 789
+
+
+const myNewArray = [200,300,400,500]
+function returnScondValue(getArray){
+    return getArray[1]
+}
+console.log(returnScondValue(myNewArray));//300
+console.log(returnScondValue([200,300,400,500]));//300
